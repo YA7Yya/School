72ddfb74b6e3166dc94d27e335d64f7455d8b88e
YA7Yya <yahyazaitoon777@gmail.com>
2023-10-17 22:47:39 +0300

    first

---
+++ script.js
@@ -0,0 +1,52 @@
+let login = document.querySelector(".login")
+function focusInput(){
+  let input = document.querySelector(".username")
+  setTimeout(() => {
+    input.focus()
+  }, 500);
+}
+login.addEventListener("click",() =>{
+  Swal.fire(
+    'Read Below !',
+    'You Can Make Login Form By Yours Because I Have Not Learnt PHP ( This Message For System Management )',
+    'error'
+  )
+})
+
+
+  // Run on page load
+  window.onload = function () {
+        
+    // If localStorage is storing default values (ex. name), exit the function and do not restore data
+    // if (localStorage.getItem('username') == "username") {
+    //   return;
+    // }
+
+
+
+
+    var username = localStorage.getItem('username');
+    if (username !== null) $('.username').val(username);
+
+    var password = localStorage.getItem('password');
+    if (password !== null) $('.password').val(password);
+
+    
+  }
+
+
+  window.onbeforeunload = function () {
+
+    localStorage.setItem("username", $('.username').val());
+    localStorage.setItem("password", $('.password').val());
+  
+  }
+  function myFunction() {
+  var show = document.getElementById("myInput");
+  if (show.type === "password") {
+      show.type = "text";
+  } else {
+      show.type = "password";
+  }
+}
+
---
+++ upload.cmd
@@ -0,0 +1,7 @@
+echo "# School" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/YA7Yya/School.git
+git push -u origin main
